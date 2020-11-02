20b80c1eb884d47963f34b197aed81eabd42fcf9
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-11-02 14:49:20 +0700

    Attempt 5 times at get before giving up

--- bnstats/bnsite/request.py
+++ bnstats/bnsite/request.py
@@ -15,9 +15,21 @@ def setup_session(session, osu_key):
     s.cookies.set(domain="bn.mappersguild.com", name="connect.sid", value=session)
 
 
-async def get(url, is_json=True):
-    r = await s.get(url)
-    r.raise_for_status()
+async def get(url, is_json=True, attempts=5):
+    current_attempt = 1
+    while current_attempt <= attempts:
+        try:
+            r = await s.get(url)
+        except BaseException as e:
+            current_attempt += 1
+
+            # Reraise if we already give too many attempts
+            if current_attempt > attempts:
+                raise e
+            continue
+
+        r.raise_for_status()
+        break
     if is_json:
         _result = r.json()
     else:
