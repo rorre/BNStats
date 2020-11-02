18812a75268e328053d0027f8ccecc655e136a33
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-11-02 14:46:30 +0700

    Don't cache map request

--- bnstats/routine.py
+++ bnstats/routine.py
@@ -13,7 +13,6 @@ from starlette.applications import Starlette
 
 from bnstats.bnsite import request
 from bnstats.bnsite.enums import MapStatus
-from bnstats.bnsite.request import cached_request as cget
 from bnstats.bnsite.request import get, s
 from bnstats.models import Beatmap, BeatmapSet, Nomination, Reset, User
 
@@ -128,7 +127,7 @@ async def update_maps_db(nomination: Nomination):
     ]:
         query = {"k": request.api_key, "s": nomination.beatmapsetId}
         url = API_URL + "/get_beatmaps?" + urlencode(query)
-        r = await cget(url, "map", f"{nomination.beatmapsetId}.json")
+        r = await get(url)
 
         db_result: List[Beatmap] = []  # type: ignore
         for map in r:
