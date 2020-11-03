0feef6e8f861c7d5803bc8850ab71ef67e3b38e8
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-11-03 07:56:06 +0700

    Remove type from request, directly use JSONArray

--- bnstats/routes/qat.py
+++ bnstats/routes/qat.py
@@ -1,15 +1,12 @@
-from datetime import datetime
-from typing import Any, Dict
+from typing import Any, Awaitable, Callable, Dict, List
 
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.routing import Router
 from starlette.config import Config
 
-from bnstats.models import User, Beatmap, Nomination, Reset
+from bnstats.models import User, Nomination, Reset
 from dateutil.parser import parse
-
-from tortoise.models import Model
 
 router = Router()
 
@@ -22,40 +19,35 @@ if DEFAULT_KEY == QAT_KEY and not DEBUG:
     raise ValueError("Cannot use default key in non-debug mode.")
 
 
-async def nomination_update(req_data: Dict[str, Any]):
-    for event in req_data["events"]:
-        db_event = await Nomination.get_or_none(
-            beatmapsetId=event["beatmapsetId"],
-            userId=event["userId"],
-        )
-        event["timestamp"] = parse(event["timestamp"], ignoretz=True)
-        event["user"] = await User.get_or_none(osuId=event["userId"])
+async def nomination_update(event: Dict[str, Any]) -> JSONResponse:
+    db_event = await Nomination.get_or_none(
+        beatmapsetId=event["beatmapsetId"],
+        userId=event["userId"],
+    )
+    event["timestamp"] = parse(event["timestamp"], ignoretz=True)
+    event["user"] = await User.get_or_none(osuId=event["userId"])
 
-        if not db_event:
-            db_event = await Nomination.create(**event)
-        else:
-            db_event.update_from_dict(event)
-            await db_event.save()
-
-    return JSONResponse({"status": 200, "message": "OK"})
+    if not db_event:
+        db_event = await Nomination.create(**event)
+    else:
+        db_event.update_from_dict(event)
+        await db_event.save()
 
 
-async def reset_update(req_data: Dict[str, Any]):
-    for event in req_data["events"]:
-        event["timestamp"] = parse(event["timestamp"], ignoretz=True)
-        db_event = await Reset.get_or_none(id=event["id"])
-        if not db_event:
-            db_event = await Reset.create(**event)
-        else:
-            db_event.update_from_dict(event)
-            await db_event.save()
-
-    return JSONResponse({"status": 200, "message": "OK"})
+async def reset_update(event: Dict[str, Any]) -> JSONResponse:
+    event["timestamp"] = parse(event["timestamp"], ignoretz=True)
+    db_event = await Reset.get_or_none(id=event["id"])
+    if not db_event:
+        db_event = await Reset.create(**event)
+    else:
+        db_event.update_from_dict(event)
+        await db_event.save()
 
 
 classes = {
     "nominate": nomination_update,
-    "reset": reset_update,
+    "nomination_reset": reset_update,
+    "disqualify": reset_update,
 }
 
 
@@ -67,15 +59,18 @@ async def new_entry(request: Request):
     ):
         return JSONResponse({"status": 401, "message": "Unauthorized."}, 401)
 
-    req_data: Dict[str, Any] = await request.json()
+    req_data: List[Dict[str, Any]] = await request.json()
 
-    data_type: str = req_data["type"]
-    if data_type not in classes.keys():
-        return JSONResponse({"status": 400, "message": "Invalid type."}, 400)
+    for event in req_data:
+        data_type: str = event["type"]
+        if data_type not in classes.keys():
+            return JSONResponse({"status": 400, "message": "Invalid type."}, 400)
 
-    func: Model = classes[data_type]
-
-    try:
-        return await func()
-    except:
-        return JSONResponse({"status": 500, "message": "An exception occured."}, 500)
+        func: Callable[[Dict[str, Any]], Awaitable] = classes[data_type]
+        try:
+            await func(event)
+        except:
+            return JSONResponse(
+                {"status": 500, "message": "An exception occured."}, 500
+            )
+    return JSONResponse({"status": 200, "message": "OK"})
