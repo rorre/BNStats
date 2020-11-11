c3f19d3d9a9c481c21163bdbc1e5c5a5d8c6e277
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-11-11 16:36:45 +0700

    Add discord hook notification support

--- populate.py
+++ populate.py
@@ -1,3 +1,4 @@
+import httpx
 import logging
 from tortoise import Tortoise, run_async
 from typing import List
@@ -17,11 +18,20 @@ config = Config(".env")
 DB_URL = config("DB_URL")
 SITE_SESSION = config("BNSITE_SESSION")
 API_KEY = config("API_KEY")
+WEBHOOK_URL = config("WEBHOOK_URL", default="")
+
 CALC_SYSTEM = get_system(config("CALC_SYSTEM"))()
 print(f"> Using calculator: {CALC_SYSTEM.name}")
 
 logger = logging.getLogger("bnstats")
 logger.setLevel(logging.DEBUG)
+
+
+def send_webhook(msg):
+    if not WEBHOOK_URL:
+        return
+    hook = {"embeds": [{"title": "BNStats Populator", "description": msg}]}
+    httpx.post(WEBHOOK_URL, json=hook)
 
 
 async def run_calculate():
@@ -36,38 +46,44 @@ async def run_calculate():
 
 
 async def run(days):
-    request.setup_session(SITE_SESSION, API_KEY)
-    await Tortoise.init(db_url=DB_URL, modules={"models": ["bnstats.models"]})
-    await Tortoise.generate_schemas()
+    send_webhook("Population starts.")
+    try:
+        request.setup_session(SITE_SESSION, API_KEY)
+        await Tortoise.init(db_url=DB_URL, modules={"models": ["bnstats.models"]})
+        await Tortoise.generate_schemas()
 
-    print("> Populating users...")
-    users: List[User] = await update_users_db()
+        print("> Populating users...")
+        users: List[User] = await update_users_db()
 
-    c = len(users)
-    for i, u in enumerate(users):
-        print(f">> Populating data for user: {u.username} ({i+1}/{c})")
-        nominations = await update_nomination_db(u, days)
+        c = len(users)
+        for i, u in enumerate(users):
+            print(f">> Populating data for user: {u.username} ({i+1}/{c})")
+            nominations = await update_nomination_db(u, days)
 
-        print(f">>> Populating maps for user: {u.username}")
-        c_maps = len(nominations)
-        for i, nom in enumerate(nominations):
-            print(f">>> Fetching: {nom.beatmapsetId} ({i+1}/{c_maps})")
-            await update_maps_db(nom)
+            print(f">>> Populating maps for user: {u.username}")
+            c_maps = len(nominations)
+            for i, nom in enumerate(nominations):
+                print(f">>> Fetching: {nom.beatmapsetId} ({i+1}/{c_maps})")
+                await update_maps_db(nom)
 
-        user_maps = []
-        all_noms = await u.get_nomination_activity()
-        for nom in all_noms:
-            m = await nom.get_map()
-            user_maps.append(m)
+            user_maps = []
+            all_noms = await u.get_nomination_activity()
+            for nom in all_noms:
+                m = await nom.get_map()
+                user_maps.append(m)
 
-        if user_maps:
-            print(f">>> Updating details for user: {u.username}")
-            await update_user_details(u, user_maps)
+            if user_maps:
+                print(f">>> Updating details for user: {u.username}")
+                await update_user_details(u, user_maps)
 
-        print(f">>> Calculating score for user: {u.username}")
-        await CALC_SYSTEM.calculate_user(u)
+            print(f">>> Calculating score for user: {u.username}")
+            await CALC_SYSTEM.calculate_user(u)
+    except BaseException as e:
+        send_webhook(f"An exception occured during population: \r\n```\r\n{str(e)}```")
+        raise e
 
     await request.s.aclose()
+    send_webhook("Population ends.")
 
 
 if __name__ == "__main__":
