5808ba85a50431026d304602891da28f4cf4696a
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-12-22 13:49:31 +0700

    Config types

--- bnstats/config.py
+++ bnstats/config.py
@@ -1,6 +1,6 @@
 from starlette.config import Config
 
-from bnstats.score import get_system
+from bnstats.score import get_system, CalculatorABC
 
 config = Config(".env")
 
@@ -9,10 +9,10 @@ SECRET: str = config("SECRET")
 DB_URL: str = config("DB_URL")
 SITE_SESSION: str = config("BNSITE_SESSION")
 API_KEY: str = config("API_KEY")
-CALC_SYSTEM = get_system(config("CALC_SYSTEM"))()
-SENTRY_URL = config("SENTRY_URL", default="")
+CALC_SYSTEM: CalculatorABC = get_system(config("CALC_SYSTEM"))()
+SENTRY_URL: str = config("SENTRY_URL", default="")
 
-INTEROP_USERNAME = config("INTEROP_USERNAME", default="")
-INTEROP_PASSWORD = config("INTEROP_PASSWORD", default="")
-USE_INTEROP = INTEROP_USERNAME and INTEROP_PASSWORD
-USE_AIESS = config("USE_AIESS", cast=bool, default=False)
+INTEROP_USERNAME: str = config("INTEROP_USERNAME", default="")
+INTEROP_PASSWORD: str = config("INTEROP_PASSWORD", default="")
+USE_INTEROP: bool = bool(INTEROP_USERNAME and INTEROP_PASSWORD)
+USE_AIESS: bool = config("USE_AIESS", cast=bool, default=False)
