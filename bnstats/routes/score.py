4c04fef12f0be000af5849e7fb3655255ede7307
Rendy Arya Kemal <renrror@gmail.com>
2023-01-05 13:22:45 +0700

    Cache score leaderboard

--- bnstats/routes/score.py
+++ bnstats/routes/score.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 from typing import Optional
 
+from dateutil.parser import parse
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
 from starlette.routing import Router
@@ -8,7 +9,7 @@ from tortoise import timezone
 from tortoise.query_utils import Q
 
 from bnstats.models import User
-from bnstats.plugins import templates
+from bnstats.plugins import templates, cache
 
 router = Router()
 
@@ -56,7 +57,7 @@ async def show_user(request: Request):
 async def leaderboard(request: Request):
     calc_system = request.scope["calculator"]
 
-    selected_mode = request.query_params.get("mode")
+    selected_mode: str = request.query_params.get("mode", "")
     is_valid_mode = selected_mode and selected_mode in [
         "osu",
         "taiko",
@@ -73,13 +74,32 @@ async def leaderboard(request: Request):
             .order_by("username")
         )
     else:
+        selected_mode = ""
         users = await User.get_users(show_former=False)
 
+    last_update = max(users, key=lambda x: x.last_updated).last_updated
+    cached_score = await cache.get(f"scores-{selected_mode}-{calc_system.name}", None)
+    has_cache = (
+        cached_score is not None and parse(cached_score["+last_update"]) >= last_update
+    )
+
+    new_cache = {}
     for u in users:
+        if has_cache and u.osuId in cached_score:
+            u.score = cached_score[u.osuId]["score"]
+            u.score_modes = cached_score[u.osuId]["score_modes"]
+            continue
+
         u.score = await u.get_score(calc_system)
         u.score_modes = {}
         for mode in u.modes:
             u.score_modes[mode] = await u.get_score(calc_system, mode=mode)
+
+        new_cache[u.osuId] = {"score": u.score, "score_modes": u.score_modes}
+
+    if not has_cache:
+        new_cache["+last_update"] = last_update.isoformat()
+        await cache.set(f"scores-{selected_mode}-{calc_system.name}", new_cache)
 
     if is_valid_mode:
         users.sort(key=lambda x: x.score_modes[selected_mode], reverse=True)
