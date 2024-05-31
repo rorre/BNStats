a1103d9eed02feaab9e6196fc288f64b5579cc7a
Rendy Arya Kemal <renrror@gmail.com>
2024-05-31 14:50:09 +0700

    fix: route moved

--- bnstats/routine/constants.py
+++ bnstats/routine/constants.py
@@ -1,3 +1,3 @@
 API_URL = "https://osu.ppy.sh/api"
-INTEROP_URL = "https://bn.mappersguild.com/interOp"
-USERS_URL = "https://bn.mappersguild.com/users"
+INTEROP_URL = "https://bn.mappersguild.com/api/interOp"
+USERS_URL = "https://bn.mappersguild.com/api/users"
